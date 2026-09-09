80acadd4de2107ebbe2edc6c89175b01c14995ce
dinesh-samant <dineshasamant@gmail.com>
2026-09-09 11:08:53 +0530

    feat(commissions): desk buttons to approve and mark paid

--- insurance_core/insurance_core/doctype/commission_payout/commission_payout.js
+++ insurance_core/insurance_core/doctype/commission_payout/commission_payout.js
@@ -1,4 +1,27 @@
 frappe.ui.form.on('Commission Payout', {
 	refresh(frm) {
-	}
-});
+		if (frm.is_new()) return
+		if (frm.doc.status === 'Accrued') {
+			frm.add_custom_button(__('Approve'), () => {
+				frappe.call({
+					method: 'insurance_core.api.approve_commission_payout',
+					args: { name: frm.doc.name },
+					callback(r) {
+						if (r.message) frm.reload_doc()
+					},
+				})
+			})
+		}
+		if (['Accrued', 'Approved'].includes(frm.doc.status)) {
+			frm.add_custom_button(__('Mark Paid'), () => {
+				frappe.call({
+					method: 'insurance_core.api.mark_commission_paid',
+					args: { name: frm.doc.name },
+					callback(r) {
+						if (r.message) frm.reload_doc()
+					},
+				})
+			}).addClass('btn-primary')
+		}
+	},
+})
