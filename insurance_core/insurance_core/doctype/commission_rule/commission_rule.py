f74077649cde2dbd8801c51d016c2958f6e184ad
dinesh-samant <dineshasamant@gmail.com>
2026-09-09 11:05:12 +0530

    feat(commissions): validate commission rules

--- insurance_core/insurance_core/doctype/commission_rule/commission_rule.py
+++ insurance_core/insurance_core/doctype/commission_rule/commission_rule.py
@@ -1,11 +1,16 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class CommissionRule(Document):
 	def validate(self):
-		self.set_missing_values()
-
-	def set_missing_values(self):
-		pass
+		if not self.rule_name:
+			frappe.throw(_("Rule Name is required."))
+		if flt(self.rate) < 0:
+			frappe.throw(_("Commission rate cannot be negative."), title=_("Invalid Rate"))
+		if flt(self.fixed_amount) < 0:
+			frappe.throw(_("Fixed amount cannot be negative."), title=_("Invalid Amount"))
+		if not flt(self.rate) and not flt(self.fixed_amount):
+			frappe.throw(_("Provide a commission rate and/or fixed amount."), title=_("Missing Rate"))
